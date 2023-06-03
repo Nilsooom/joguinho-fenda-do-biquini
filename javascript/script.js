5511bc98db74b8c4f709f1dbba5d784442905754
Nilsooom <nilsooom.b@gmail.com>
2023-06-03 16:24:58 -0300

    Adicionado botões para iniciar e reiniciar o jogo

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,8 @@
 const patrick = document.querySelector('.patrick')
 const aguaViva = document.querySelector('.obstaculo')
 const gameOver = document.querySelector('.fundo')
+const inicio = document.getElementById('play')
+const reiniciar = document.getElementById('restart')
 
 
 let jogoAcabou = false
@@ -22,36 +24,59 @@ function morte (){
     }, 2000)
 }
 
-const loop = setInterval(()=>{
-  const posicaoObstaculo = aguaViva.offsetLeft // distancia do obstaculo a esquerda
-  const posicaoPatrick = +window.getComputedStyle(patrick).bottom.replace('px', '') // referencia o pulo do bob
+function jogo (){
 
-  if (posicaoObstaculo <= 235  && posicaoObstaculo > 0 && posicaoPatrick < 82){
-    jogoAcabou = true
-    aguaViva.style.animation = 'none' // para a animação
-    // aguaViva.style.left = `${posicaoObstaculo}px` // para o obstaculo parar na posição que ele estava
+  aguaViva.style.display = 'block'
 
-    
-    patrick.style.animation = 'none'
-    patrick.style.bottom = `${posicaoPatrick}px`
-    patrick.src = "./imagens/patrick-dead.png" // troca para animação de morte
-    patrick.style.width = '120px'
-    patrick.style.left = 'calc(50% - 100px)';
-    
-    morte()
-    gameOver.src = "./imagens/game-over.gif"
-    gameOver.style.width = '100%'
-    gameOver.style.height = '400px'
+  const loop = setInterval(()=>{
+    const posicaoObstaculo = aguaViva.offsetLeft // distancia do obstaculo a esquerda
+    const posicaoPatrick = +window.getComputedStyle(patrick).bottom.replace('px', '') // referencia o pulo do bob
 
-// Adicione a classe hidden ao obstáculo e ao Patrick para sumir.
-aguaViva.classList.add('hidden'); 
-    setTimeout(()=>{
-      patrick.classList.add('hidden');
-    }, 1500)
+    if (posicaoObstaculo <= 235  && posicaoObstaculo > 0 && posicaoPatrick < 82){
+      jogoAcabou = true
+      aguaViva.style.animation = 'none' // para a animação do obstaculo
+      aguaViva.style.display = 'none' // esconde o obstaculo
+      patrick.style.animation = 'none'
+      patrick.style.bottom = `${posicaoPatrick}px`
+      patrick.src = "./imagens/patrick-dead.png" // troca para animação de morte
+      patrick.style.width = '120px'
+      patrick.style.left = 'calc(50% - 100px)';
+      
+      morte()
+      gameOver.src = "./imagens/game-over.gif"
+      gameOver.style.width = '100%'
+      gameOver.style.height = '400px'
+  
+  // Adicione a classe hidden ao obstáculo e ao Patrick para sumir.
+      setTimeout(()=>{
+        patrick.classList.add('hidden');
+      }, 1500)
+  
+      clearInterval(loop)
+    }
+  }, 10)
+}
 
-    clearInterval(loop)
-  }
-}, 10)
+// function reiniciarJogo() {
+//   jogoAcabou = false;
+
+//   aguaViva.classList.remove('hidden'); 
+//   patrick.classList.remove('hidden');
+//   aguaViva.style.animation = 'obstaculo-animacao 1s linear infinite';
+//   patrick.style.bottom = '-1px';
+  
+//   patrick.classList.add('');
+//   patrick.src = "./imagens/patrick-star.gif";
+//   patrick.style.width = '140px';
+//   patrick.style.left = '100px';
+//   gameOver.src = "./imagens/fundo-do-mar.jpg";
+//   gameOver.style.width = '100%';
+//   gameOver.style.height = '400px';
+// } projeto furuto.
 
 
+reiniciar.onclick = ()=>{
+  location.reload()
+}
+inicio.addEventListener('click', jogo) 
 document.addEventListener('keydown', pular)
